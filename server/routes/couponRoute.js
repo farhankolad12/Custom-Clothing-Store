df9ccbf93fae5267c254c01ce891af50b356d3b5
Farhan Kolad <98447917+farhankolad12@users.noreply.github.com>
2024-05-05 22:54:13 +0530

    Update couponRoute.js

--- server/routes/couponRoute.js
+++ server/routes/couponRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const Multer = require("multer");
 
-const { isAuthenticate } = require("../middlewares/auth");
+const { isAuthenticate, authorizeRoles } = require("../middlewares/auth");
 const {
   addUpdateCoupon,
   getCoupons,
@@ -26,9 +26,9 @@ router.use(
   })
 );
 
-router.route("/coupon").post(isAuthenticate, upload.any(), addUpdateCoupon);
-router.route("/coupons").get(isAuthenticate, getCoupons);
-router.route("/remove-coupon").post(isAuthenticate, deleteCoupon);
+router.route("/coupon").post(isAuthenticate, authorizeRoles("admin"), upload.any(), addUpdateCoupon);
+router.route("/coupons").get(isAuthenticate, authorizeRoles("admin"), getCoupons);
+router.route("/remove-coupon").post(isAuthenticate, authorizeRoles("admin"), deleteCoupon);
 router.route("/check-code").post(isAuthenticate, checkCode);
 
 module.exports = router;
