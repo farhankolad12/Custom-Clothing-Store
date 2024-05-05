9b76185f0a99d98c37669edbef703429dffc70d5
Farhan Kolad <98447917+farhankolad12@users.noreply.github.com>
2024-05-05 22:56:45 +0530

    Update categoryRoute.js

--- server/routes/categoryRoute.js
+++ server/routes/categoryRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const Multer = require("multer");
 
-const { isAuthenticate } = require("../middlewares/auth");
+const { isAuthenticate, authorizeRoles } = require("../middlewares/auth");
 const {
   addCategory,
   getCategories,
@@ -25,8 +25,8 @@ router.use(
   })
 );
 
-router.route("/category").post(isAuthenticate, upload.any(), addCategory);
-router.route("/category").get(isAuthenticate, getCategories);
-router.route("/remove-category").post(isAuthenticate, deleteCategory);
+router.route("/category").post(isAuthenticate,authorizeRoles("admin"), upload.any(), addCategory);
+router.route("/category").get(isAuthenticate, authorizeRoles("admin"), getCategories);
+router.route("/remove-category").post(isAuthenticate, authorizeRoles("admin"), deleteCategory);
 
 module.exports = router;
