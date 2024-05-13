96c530375030926ced45950b8bda24947813b5c6
Farhan <farhankolad@gmail.com>
2024-05-13 16:48:29 +0530

    SEO Updated

--- client/endusers/app/robots.ts
+++ client/endusers/app/robots.ts
@@ -1,52 +1,13 @@
 import { MetadataRoute } from "next";
 
-import { ProductType } from "./definations";
-
-const EXTERNAL_DATA_URL1 =
-  process.env.NEXT_PUBLIC_BACKEND_HOSTNAME + "/products-sitemap";
-
-const EXTERNAL_DATA_URL2 =
-  process.env.NEXT_PUBLIC_BACKEND_HOSTNAME + "/blogs-sitemap";
-
 export default async function robots(): Promise<MetadataRoute.Robots> {
-  const res1 = await fetch(EXTERNAL_DATA_URL1, {
-    credentials: "include",
-    method: "GET",
-  });
-  const products: ProductType[] = await res1.json();
-
-  const res2 = await fetch(EXTERNAL_DATA_URL2, {
-    credentials: "include",
-    method: "GET",
-  });
-  const blogs: any[] = await res2.json();
-
-  const staticRules = [
-    { userAgent: "*", allow: "/" },
-    { userAgent: "*", allow: "/about" },
-    { userAgent: "*", allow: "/contact" },
-    { userAgent: "*", allow: "/shop" },
-    { userAgent: "*", allow: "/blogs" },
-    { userAgent: "*", disallow: "/cart" },
-    { userAgent: "*", disallow: "/profile" },
-    { userAgent: "*", disallow: "/wishlist" },
-  ];
-
   return {
     rules: [
-      ...staticRules,
-      ...products?.map((product) => {
-        return {
-          userAgent: "*",
-          allow: "/product/" + product._id,
-        };
-      }),
-      ...blogs?.map((blog) => {
-        return {
-          userAgent: "*",
-          allow: "/blog/" + blog._id,
-        };
-      }),
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/cart", "/profile", "/wishlist"],
+      },
     ],
     sitemap: "https://www.essentialsbyla.com/sitemap.xml",
   };
