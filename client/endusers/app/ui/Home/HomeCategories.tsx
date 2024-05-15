48dd5ae800ade33b16c71df80d772f9789f044e7
Farhan <farhankolad@gmail.com>
2024-05-15 20:50:27 +0530

    Design Changed

--- client/endusers/app/ui/Home/HomeCategories.tsx
+++ client/endusers/app/ui/Home/HomeCategories.tsx
@@ -8,7 +8,7 @@ export default function HomeCategories() {
   const { data } = useAuth();
 
   return (
-    <div className="flex lg:overflow-hidden lg:px-0 md:ps-[5rem] ps-[20rem] md:pe-[5rem] sm:pe-[2rem] overflow-x-scroll w-[auto] justify-center gap-[2rem] items-center my-[5rem]">
+    <div className="flex lg:overflow-hidden lg:px-0 md:ps-[5rem] px-[2rem] md:pe-[5rem] sm:pe-[2rem] overflow-x-scroll w-[auto] lg:justify-center md:justify-center justify-start gap-[2rem] lg:items-center md:items-center items-start my-[5rem]">
       {data?.categories?.map((category: any) => {
         return (
           <Link
